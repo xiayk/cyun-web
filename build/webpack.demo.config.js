5596ee5f1d03ad656caab9c44b2b2aa19622ab96
XiaYk <xiayk@xiayk.cn>
2019-11-26 13:12:18 +0800

    修改路径

--- build/webpack.demo.config.js
+++ build/webpack.demo.config.js
@@ -18,7 +18,7 @@ fs.open("./build/env.js", "w", function(err, fd) {
 
 module.exports = merge(webpackBaseConfig, {
     output: {
-        publicPath: "/xlink/dist/", // 修改 https://iv...admin 这部分为你的服务器域名
+        publicPath: "/cyun-web/dist/", // 修改 https://iv...admin 这部分为你的服务器域名
         filename: "[name].[hash].js",
         chunkFilename: "[name].[hash].chunk.js"
     },
